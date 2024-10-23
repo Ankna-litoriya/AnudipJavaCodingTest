3625609c68e43750ddd62a4ff079dd0e5bd05ddb
Ankna Litoriya <109315012+Ankna-litoriya@users.noreply.github.com>
2024-10-23 21:48:50 +0530

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,25 +1,26 @@
+// To reverse a given string
+
 package CodingInterview;
 import java.util.Scanner;
 
 public class ReverseString {
 
 	public static void main(String args[]) {
-		//to take input
+
 		Scanner scanner = new Scanner(System.in);
 		
 		String str = scanner.nextLine();
 		String reversed = "";
 		
-		//Loop to iterate the string 
 		for (int i = str.length() - 1; i>=0; i--) {
 			reversed = reversed + str.charAt(i);
 		}
 		
 		System.out.println(reversed);
 
-                //Closing the scnner object to prevent resource leaks.
+
                 scanner.close();
 	}
 }
 
-// To reverse a given string
+
